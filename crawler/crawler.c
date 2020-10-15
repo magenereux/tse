c6766ec5b37361570d41660da65353852ef01712
Marguerite Genereux <marguerite.a.generux.21@dartmouth.edu>
2020-10-15 12:16:18 -0400

    10/15 crawler edits

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -13,5 +13,29 @@
 
 int main(void){
 	printf("hello\n");
-	return 0;
+	char *url1 = "https://thayer.github.io/engs50/";
+	webpage_t* wp1 = webpage_new(url1, 0, NULL;
+
+	if (webpage_fetch(wp1)){
+		char *html1 = webpage_getHTML(wp1);
+		int pos = 0;
+ 		char *result;
+
+ 		while ((pos = webpage_getNextURL(page, pos, &result)) > 0) {
+		bool internal = IsInternalURL(char *result);
+		printf("Found url: %s ", result);
+		if (internal){
+			printf("is internal.\n");
+		}
+		else{
+			printf("is external.\n");			
+		}
+		free(result);
+		free(wp1);
+		exit(EXIT_SUCCESS);
+	}
+	else{
+		free(wp1);
+		exit(EXIT_FAILURE);
+	}
 }
