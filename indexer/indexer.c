3662aaf31eda95fa2ca77107dbbdc9b1d28ada15
Sherrina Hwang <sherrina.y.hwang.21@dartmouth.edu>
2020-10-23 16:55:26 -0400

    10/23 afternoooon

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -15,6 +15,13 @@
 #include "webpage.h"
 #include "hash.h"
 
+int sum=0;
+
+typedef struct indexed {
+	char *key;
+	int count;
+} indexed_t;
+
 int NormalizeWord(char *word) {
 	if (strlen(word)<3)
 		return 1;
@@ -28,54 +35,55 @@ int NormalizeWord(char *word) {
 	return 0;
 }
 
-bool searchfn(void *word, const void* searchword) {
-	char *w1=(char*)word;
+bool searchfn(void *indx, const void* searchword) {
+	indexed_t *w1=(indexed_t *)indx;
+	//printf("w1=%s\n",w1->key);
 	char *w2=(char*)searchword;
-	if (strcmp(w1,w2)==0)
+	//printf("w2=%s\n",w2);
+	if (strcmp(w1->key,w2)==0)
 		return true;
 	return false;
 }
-/*
-void sumofCounts(void *indexed) {
-	*sum+=(indexed_t*)indexed->count;
+
+void sumWords(void* ip) {
+	//printf("in sumwords!");
+	sum=sum+((indexed_t*)ip)->count;
+	//printf("%d\n",((indexed_t*)ip)->count);
 }
-*/
-typedef struct indexed {
-	char *word;
-	int count;
-} indexed_t;
 
 int main (void) {
 	webpage_t *wp=pageload(1,"pages");
 	char *word;
 	int pos=0;
 	hashtable_t *htp=hopen(webpage_getHTMLlen(wp));
-	//	indexed_t *target=malloc(sizeof(indexed_t));
-	int counter=0;
+
 	while ((pos=webpage_getNextWord(wp,pos,&word))>0) {
-	  if (NormalizeWord(word)==0) {
+		//printf("word: %s\n",word);
+		if (NormalizeWord(word)==0) {
 			indexed_t *target=(indexed_t*)hsearch(htp,searchfn,word,strlen(word));
+			printf("target: %s\n",(char*)target);
 			if (target==NULL) {
 				indexed_t *indx=malloc(sizeof(indexed_t));
-				indx->word=word;
+				strcpy(indx->key,word);
 				indx->count=0;
-				hput(htp,word,word,strlen(word));
-				counter++;
+				hput(htp,indx,word,strlen(word));			
 			} else {
-				target->count++;
+				target->count=(target->count)+1;
+				//printf("%s\n",target->word);
 			}
+		
 		free(word);
 		}
 	}
 
 	// to print the total sum of counts
-	int sum=0;
-	//happly(htp,sumofCounts;
+	happly(htp,sumWords);
+				 /*
 	int toAdd=0;
 	for (int i=0;i<counter;i++) {
-		toAdd=((indexed_t*)htp[i]->count);
+		toAdd=htp[i]->count;
 		sum+=toAdd;
-	}
+		} */
 	printf("sum: %d\n",sum);
 
 	webpage_delete(wp);
