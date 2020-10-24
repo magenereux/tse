d662e214b283438b5888462ea587e4550d996e73
Sherrina Hwang <sherrina.y.hwang.21@dartmouth.edu>
2020-10-24 18:43:39 -0400

    shwang 7pm et

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -87,50 +87,49 @@ void closeQueue(void *ip){
 	p->Docs=NULL;
 }
 
-int main (void) {
-	int ID=1;
-	webpage_t *wp=pageload(ID,"pages");
+int main (int argc, char* argv[]) {
+	int endID=atoi(argv[1]);
 	char *word;
 	int pos=0;
 	hashtable_t *htp=hopen(100);
-	//open queue of words in index queue_t *q=qopen();
-	//qput(q,index);
-	while ((pos=webpage_getNextWord(wp,pos,&word))>0) {
-		if (NormalizeWord(word)==0) {
-			wordCount_t *target=(wordCount_t*)hsearch(htp,searchfn,word,strlen(word));
-			if (target==NULL) {
-				//printf("hi");
-				wordCount_t *indx=malloc(sizeof(wordCount_t));
-				docCount_t *dc = malloc(sizeof(docCount_t));
-				indx->key = word;	
-				indx->Docs=qopen();
-				dc->DocID = ID;
-				dc->count = 1;
-				//printf("%d %d\n",dc->DocID,dc->count);
-				qput(indx->Docs,dc);
-				//docCount_t *targetDoc=qget(indx->Docs);
-				//printf("%d %d\n",targetDoc->DocID,targetDoc->count);
-				hput(htp,(void *)indx,word,strlen(word));
-				//printf("after put\n");		
-			}
-			else {
-				docCount_t *targetDoc=(docCount_t*)qsearch(target->Docs,searchID,&ID);
-				//printf("target ID:%d Count:%d\n",targetDoc->DocID,targetDoc->count);
-				targetDoc->count=(targetDoc->count)+1;
-				free(word);
+
+	for (int ID=1;ID<=endID;ID++) {
+		webpage_t *wp=pageload(ID,"pages");
+		while ((pos=webpage_getNextWord(wp,pos,&word))>0) {
+			if (NormalizeWord(word)==0) {
+				wordCount_t *target=(wordCount_t*)hsearch(htp,searchfn,word,strlen(word));
+				if (target==NULL) {
+					wordCount_t *indx=malloc(sizeof(wordCount_t));
+					docCount_t *dc = malloc(sizeof(docCount_t));
+					indx->key = word;	
+					indx->Docs=qopen();
+					dc->DocID = ID;
+					dc->count = 1;
+					qput(indx->Docs,dc);
+					hput(htp,(void *)indx,word,strlen(word));
+				}	else {
+					docCount_t *targetDoc=(docCount_t*)qsearch(target->Docs,searchID,&ID);
+					if (targetDoc==NULL) {
+						docCount_t *dc = malloc(sizeof(docCount_t));
+						dc->DocID = ID;
+						dc->count = 1;
+						qput(target->Docs,dc);
+					} else {			
+						targetDoc->count=(targetDoc->count)+1;
+					}
+					free(word);
+				}
 			}
 		}
+		webpage_delete(wp);
+		pos=0;
 	}
-	
-//pageload every file in pages>create a hashtable/index of words in file
-//put those entries into queue? 
 
-	// to print the total sum of counts
 	happly(htp,sumWords);
 	printf("sum: %d\n",sum);
+
 	happly(htp,freeWords);
 	happly(htp,closeQueue);
-	webpage_delete(wp);
 	hclose(htp);
   exit(EXIT_SUCCESS);
 }
--- indexer/step4indexer.c
+++ indexer/step4indexer.c
@@ -89,48 +89,37 @@ void closeQueue(void *ip){
 
 int main (void) {
 	int ID=1;
-	webpage_t *wp=pageload(ID,"pages");
 	char *word;
 	int pos=0;
 	hashtable_t *htp=hopen(100);
-	//open queue of words in index queue_t *q=qopen();
-	//qput(q,index);
+	webpage_t *wp=pageload(ID,"pages");
+
 	while ((pos=webpage_getNextWord(wp,pos,&word))>0) {
 		if (NormalizeWord(word)==0) {
 			wordCount_t *target=(wordCount_t*)hsearch(htp,searchfn,word,strlen(word));
 			if (target==NULL) {
-				//printf("hi");
 				wordCount_t *indx=malloc(sizeof(wordCount_t));
 				docCount_t *dc = malloc(sizeof(docCount_t));
 				indx->key = word;	
 				indx->Docs=qopen();
 				dc->DocID = ID;
 				dc->count = 1;
-				//printf("%d %d\n",dc->DocID,dc->count);
 				qput(indx->Docs,dc);
-				//docCount_t *targetDoc=qget(indx->Docs);
-				//printf("%d %d\n",targetDoc->DocID,targetDoc->count);
 				hput(htp,(void *)indx,word,strlen(word));
-				//printf("after put\n");		
-			}
-			else {
-				docCount_t *targetDoc=(docCount_t*)qsearch(target->Docs,searchID,&ID);
-				//printf("target ID:%d Count:%d\n",targetDoc->DocID,targetDoc->count);
+			}	else {
+				docCount_t *targetDoc=(docCount_t*)qsearch(target->Docs,searchID,&ID);			
 				targetDoc->count=(targetDoc->count)+1;
 				free(word);
 			}
 		}
 	}
 	
-//pageload every file in pages>create a hashtable/index of words in file
-//put those entries into queue? 
 
-	// to print the total sum of counts
 	happly(htp,sumWords);
 	printf("sum: %d\n",sum);
+	webpage_delete(wp);
 	happly(htp,freeWords);
 	happly(htp,closeQueue);
-	webpage_delete(wp);
 	hclose(htp);
   exit(EXIT_SUCCESS);
 }
