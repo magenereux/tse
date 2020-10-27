376b41c1e709cbae2449801d09d3a51aa5030ee4
Carson Levine <carsonglevine@me.com>
2020-10-27 13:32:39 -0400

    progress on step 6, not there yet C&S Tues 1pm EST

---
+++ tester/indexiotest.c
@@ -0,0 +1,29 @@
+/* indexiotest.c --- 
+ * 
+ * 
+ * Author: Sherrina Y. Hwang
+ * Created: Wed Oct 21 22:01:16 2020 (-0400)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "pageio.h"
+#include "webpage.h"
+#include "indexio.h"
+
+int main (void) {
+	hashtable_t *ip=indexload(3,"index_testfiles");
+    indexsave(ip,3, "indexnm");
+
+	hashtable_t *ip2=indexload(3,"indexnm");
+
+    system("diff ./../index_testfiles/3 ./../indexnm/3");
+
+    hclose(ip);
+    hclose(ip2);
+
+	return 0;
+}
