2f696fcf760d5b54b1e05ee36b16187e3936bfc3
Rachelle Louie <Rachelle.A.Louie.21@dartmouth.edu>
2020-10-27 21:08:33 -0400

    Elle tues 9pm

--- tester/indexiotest.c
+++ tester/indexiotest.c
@@ -16,16 +16,17 @@
 
 int main (void) {
 	printf("entering main");
-    indexload(3,"index_testfiles");
-    //hashtable_t *ip=
-    // indexsave(ip,3, "indexnm");
+    
+    hashtable_t *ip=indexload(3,"index_testfiles");
+    indexsave(ip,3, "indexnm");
 
-	// hashtable_t *ip2=indexload(3,"indexnm");
+	//hashtable_t *ip2= hopen(100);
+    //ip2=indexload(3,"indexnm");
 
     //system("diff ./../index_testfiles/3 ./../indexnm/3");
-
-    // hclose(ip);
-    // hclose(ip2);
+    
+    hclose(ip);
+    //hclose(ip2);
 
 	return 0;
 }
