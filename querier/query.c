a0700d2c68b76bf7a1bc2b15d8518eda903403a2
Rachelle Louie <Rachelle.A.Louie.21@dartmouth.edu>
2020-10-30 16:25:03 -0400

    Elle friday 4pm

--- querier/query.c
+++ querier/query.c
@@ -18,12 +18,17 @@ int main(int argc, char* argv[]){
     char input[80];
     printf(">");
     int invalid=0;
-    
-while(scanf(" %99[^\n]s",input)!=EOF){
-    
+/*scanf(" %c",input);
+if(input[0]==13)
+    printf("RETURN");
+*/
+
+while(scanf(" %[^\n]",input)!=EOF ){
+    if(input[0]=='\r')
+        printf("RETURN\n");
     char *token= strtok(input," ");
-    printf("input:%d",token[0]);
-    if (input[0]!= '\n'){
+    //printf("input:%d",token[0]);
+    if (input[0]!= '\r'){
         while (token!=NULL){
             for(int i=0; i<strlen(token);i++){
                 if (isalpha(token[i])==0){  
@@ -32,11 +37,13 @@ while(scanf(" %99[^\n]s",input)!=EOF){
                 token[i]=tolower(token[i]);
                 }
             }
-            if(invalid>0)
-                printf("[invalid query]");
-            else
+            if(invalid==0)
                 printf("%s",token);
             token = strtok(NULL," ");
+        }
+        if(invalid>0){
+            printf("[invalid query]");
+            invalid=0;
         }
     }
     printf("\n");
